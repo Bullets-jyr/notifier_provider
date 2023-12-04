cf54f158ccf009885795acbe8ea05edc3cce7393
Bullets-jyr <jyr@bullets.co.kr>
2023-12-04 16:27:26 +0900

    37. NotifierProvider - counterProvider(autoDispose, family)

--- lib/pages/counter/counter_page.dart
+++ lib/pages/counter/counter_page.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'counter_provider.dart';
 
 class CounterPage extends ConsumerWidget {
   const CounterPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // final counter = ref.watch(counterProvider);
+    final counter = ref.watch(counterProvider(10));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter'),
       ),
-      body: const Center(
-        child: Text('Counter'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {
+                // ref.read(counterProvider.notifier).increment();
+                ref.read(counterProvider(10).notifier).increment();
+              },
+              child: Text(
+                'Increment',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
