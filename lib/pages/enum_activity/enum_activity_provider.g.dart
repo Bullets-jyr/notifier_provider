1c16b47fbdef194f0b499081dea555ccdbc7f432
Bullets-jyr <jyr@bullets.co.kr>
2023-12-04 18:17:56 +0900

    41. NotifierProvider - EnumActivity, listen, invalidate, provider rebuild

--- lib/pages/enum_activity/enum_activity_provider.g.dart
+++ lib/pages/enum_activity/enum_activity_provider.g.dart
@@ -6,7 +6,21 @@ part of 'enum_activity_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$enumActivityHash() => r'dfb31d58cc4d087e272d373f21b91a804393fa15';
+String _$myCounterHash() => r'08aa79aa72de3999f55247db1de2cabfa13c9357';
+
+/// See also [MyCounter].
+@ProviderFor(MyCounter)
+final myCounterProvider = AutoDisposeNotifierProvider<MyCounter, int>.internal(
+  MyCounter.new,
+  name: r'myCounterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myCounterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MyCounter = AutoDisposeNotifier<int>;
+String _$enumActivityHash() => r'a1e8c8d2ecfcfc0d03dc08aff082e1b583dbcdaf';
 
 /// See also [EnumActivity].
 @ProviderFor(EnumActivity)
