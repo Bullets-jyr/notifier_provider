f10eba3ecbda86c949f31811b948f956c94f3424
Bullets-jyr <jyr@bullets.co.kr>
2023-12-05 11:13:02 +0900

    45. NotifierProvider - sealedAsyncActivityProvider

---
+++ lib/pages/sealed_async_activity/sealed_async_activity_provider.dart
@@ -0,0 +1,48 @@
+import 'sealed_async_activity_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../models/activity.dart';
+import '../../providers/dio_provider.dart';
+
+part 'sealed_async_activity_provider.g.dart';
+
+@riverpod
+class SealedAsyncActivity extends _$SealedAsyncActivity {
+  int _errorCounter = 0;
+
+  SealedAsyncActivity() {
+    print('[SealedAsyncActivty] constructor called');
+  }
+
+  @override
+  SealedAsyncActivityState build() {
+    print('[sealedAsyncActivityProvider] initialized');
+    ref.onDispose(() {
+      print('[sealedAsyncActivityProvider] disposed');
+    });
+    print('hashCode: $hashCode');
+    fetchActivity(activityTypes[0]);
+    return const SealedAsyncActivityLoading();
+  }
+
+  Future<void> fetchActivity(String activityType) async {
+    print('hashCode in fetchActivity: $hashCode');
+    state = const SealedAsyncActivityLoading();
+
+    try {
+      print('_errorCounter: $_errorCounter');
+      if (_errorCounter++ % 2 != 1) {
+        await Future.delayed(const Duration(milliseconds: 500));
+        throw 'Fail to fetch activity';
+      }
+
+      final response = await ref.read(dioProvider).get('?type=$activityType');
+
+      final activity = Activity.fromJson(response.data);
+
+      state = SealedAsyncActivitySuccess(activity: activity);
+    } catch (e) {
+      state = SealedAsyncActivityFailure(error: e.toString());
+    }
+  }
+}
