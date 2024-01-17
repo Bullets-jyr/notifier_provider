05e5c54e4d2f4bd2dc17787030cf3cc60667f780
Bullets-jyr <jyr@bullets.co.kr>
2024-01-17 12:15:00 +0900

    36. NotifierProvider - counterProvider

---
+++ lib/pages/counter/counter_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Counter extends Notifier<int> {
+  @override
+  int build() {
+    ref.onDispose(() {
+      print('[counterProvider] disposed');
+    });
+    return 0;
+  }
+
+  void increment() {
+    state++;
+  }
+}
+
+// final counterProvider = NotifierProvider<Counter, int>(() {
+//   return Counter();
+// });
+
+// constructor tear-offs
+final counterProvider = NotifierProvider<Counter, int>(Counter.new);
