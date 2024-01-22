326da0991bc4b07219e6533a36ebbc544085f12b
Bullets-jyr <jyr@bullets.co.kr>
2024-01-22 17:05:07 +0900

    40. NotifierProvider - EnumActivityPage

---
+++ lib/pages/enum_activity/enum_activity_page.dart
@@ -0,0 +1,110 @@
+import 'dart:math';
+
+import 'package:bulleted_list/bulleted_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../models/activity.dart';
+import 'enum_activity_provider.dart';
+import 'enum_activity_state.dart';
+
+class EnumActivityPage extends ConsumerStatefulWidget {
+  const EnumActivityPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _EnumActivityPageState();
+}
+
+class _EnumActivityPageState extends ConsumerState<EnumActivityPage> {
+  @override
+  void initState() {
+    super.initState();
+    // API 호출
+    ref.read(enumActivityProvider.notifier).fetchActivity(activityTypes[0]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final activityState = ref.watch(enumActivityProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('EnumActivityNotifier'),
+      ),
+      body: switch (activityState.status) {
+        ActivityStatus.initial => const Center(
+          child: Text(
+            'Get some activity',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        ActivityStatus.loading => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        ActivityStatus.failure => Center(
+          child: Text(
+            activityState.error,
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.red,
+            ),
+          ),
+        ),
+        ActivityStatus.success => ActivityWidget(
+          activity: activityState.activity,
+        ),
+      },
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          final randomNumber = Random().nextInt(activityTypes.length);
+          ref
+              .read(enumActivityProvider.notifier)
+              .fetchActivity(activityTypes[randomNumber]);
+        },
+        label: Text(
+          'New Activity',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
+
+class ActivityWidget extends StatelessWidget {
+  final Activity activity;
+  const ActivityWidget({
+    Key? key,
+    required this.activity,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25),
+      child: Column(
+        children: [
+          Text(
+            activity.type,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const Divider(),
+          BulletedList(
+            bullet: const Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+            listItems: [
+              'activity: ${activity.activity}',
+              'accessibility: ${activity.accessibility}',
+              'participants: ${activity.participants}',
+              'price: ${activity.price}',
+              'key: ${activity.key}',
+            ],
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ],
+      ),
+    );
+  }
+}
