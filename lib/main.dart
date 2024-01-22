e4e282d7a3aec86c5426f618023a52b28fe8711d
Bullets-jyr <jyr@bullets.co.kr>
2024-01-22 20:11:02 +0900

    43. NotifierProvider - SealedActivityPage

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'pages/counter/counter_page.dart';
 import 'pages/enum_activity/enum_activity_page.dart';
+import 'pages/sealed_activity/sealed_activity_page.dart';
 import 'widgets/custom_button.dart';
 
 void main() {
@@ -51,6 +52,10 @@ class MyHomePage extends StatelessWidget {
             CustomButton(
               title: 'Enum Activity',
               child: EnumActivityPage(),
+            ),
+            CustomButton(
+              title: 'Sealed Activity',
+              child: SealedActivityPage(),
             ),
           ],
         ),
