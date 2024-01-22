3ae37560200e58aefc4956fe7f12edf994ce13f6
Bullets-jyr <jyr@bullets.co.kr>
2024-01-22 18:04:54 +0900

    42. NotifierProvider - SealedActivityState, sealedActivityProvider

---
+++ lib/pages/sealed_activity/sealed_activity_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sealed_activity_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sealedActivityHash() => r'90cee0963b85223661437a1ac9b08d2bb473eeb5';
+
+/// See also [SealedActivity].
+@ProviderFor(SealedActivity)
+final sealedActivityProvider =
+    AutoDisposeNotifierProvider<SealedActivity, SealedActivityState>.internal(
+  SealedActivity.new,
+  name: r'sealedActivityProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$sealedActivityHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SealedActivity = AutoDisposeNotifier<SealedActivityState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
